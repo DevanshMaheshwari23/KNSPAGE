5b4c60f9aa2c42d85ef46d475c5ae9b7936bc249
Devansh Maheshwari <devanshmaheshwari@Devanshs-MacBook-Air.local>
2025-01-20 09:05:18 +0530

    Initial Setup Complete

--- dist/bundle.js
+++ dist/bundle.js
@@ -83294,30 +83294,70 @@ var __webpack_exports__ = {};
   !*** ./app.js ***!
   \****************/
 var __dirname = "/";
+// const express=require("express");
+// const app=express();
+// const path=require("path");
+// const port=5500;
+
+// app.set("view engine","ejs");
+
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.static('public'));
+
+// app.get(("/form"),(req, res)=>{
+//     res.render("form.ejs");
+
+// });
+// app.get(("/thankyou"),(req,res)=>{
+//     res.render("thankyou.ejs");
+// });
+
+// app.get("/",(req,res)=>{
+//     res.sendFile("index.html"),{root:__dirname};
+// });
+// // ,{root:__dirname}
+
+// app.listen(port,()=>{
+//     console.log("App is listining on port 8080");
+// });
 var express = __webpack_require__(/*! express */ "./node_modules/express/index.js");
 var app = express();
 var path = __webpack_require__(/*! path */ "./node_modules/path-browserify/index.js");
-var port = 5500;
+
+// Set the port
+var port = process.env.PORT || 5500; // Vercel sets PORT in the environment variables
+
+// Set the view engine to ejs
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to parse URL-encoded data
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(express["static"]('public'));
+
+// Middleware to serve static files from the 'public' folder
+app.use(express["static"](path.join(__dirname, 'public')));
+
+// Route for the form page
 app.get("/form", function (req, res) {
   res.render("form.ejs");
 });
+
+// Route for the thank you page
 app.get("/thankyou", function (req, res) {
   res.render("thankyou.ejs");
 });
-app.get("/", function (req, res) {
-  res.sendFile("index.html"), {
-    root: __dirname
-  };
-});
-// ,{root:__dirname}
 
+// Route for the homepage
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Start the server on port 5500
 app.listen(port, function () {
-  console.log("App is listining on port 8080");
+  console.log("App is listening on port ".concat(port));
 });
 })();
 
